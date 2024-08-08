b43a931365064e6f54823cfd311d3d6809c55ade
Matt Rodgers <mattrodgers31@gmail.com>
2024-08-08 08:34:16 +0100

    use set intersection

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -15,7 +15,7 @@ pub fn run() -> Result<Answer> {
             let winning: HashSet<&str> = tmp[1].split(' ').filter(|num| !num.is_empty()).collect();
             let ours: HashSet<&str> = tmp[2].split(' ').filter(|num| !num.is_empty()).collect();
 
-            let win_count: u32 = ours.iter().filter(|&num| winning.contains(num)).count() as u32;
+            let win_count: u32 = ours.intersection(&winning).count() as u32;
 
             2u64.pow(win_count - 1)
         })
