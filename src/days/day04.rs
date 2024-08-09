495f7b038d1b554b53e05adcce5956814646f06c
Matt Rodgers <mattrodgers31@gmail.com>
2024-08-09 18:01:36 +0100

    day04

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,25 +1,40 @@
 use crate::util::answer::*;
 use crate::util::input;
 use anyhow::Result;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn run() -> Result<Answer> {
-    let pt2 = 0;
-
     let lines = input::get_lines("inputs/04.in")?;
+    let mut card_count: HashMap<usize, u64> = (0..lines.len()).map(|i| (i, 1)).collect();
 
     let pt1: u64 = lines
-        .iter()
-        .map(|line| {
+        .iter().enumerate()
+        .map(|(i, line)| {
             let tmp: Vec<&str> = line.split([':', '|']).collect();
             let winning: HashSet<&str> = tmp[1].split(' ').filter(|num| !num.is_empty()).collect();
             let ours: HashSet<&str> = tmp[2].split(' ').filter(|num| !num.is_empty()).collect();
 
-            let win_count: u32 = ours.intersection(&winning).count() as u32;
+            let win_count = ours.intersection(&winning).count();
 
-            2u64.pow(win_count - 1)
+            /* Part 2: count up the won cards */
+            for j in 0..win_count{
+                let won_card = i + j + 1;
+                match card_count.get(&won_card) {
+                    Some(count) => { card_count.insert(won_card, count + card_count.get(&i).expect("index not in card_count")); },
+                    None        => { panic!("index not in card_count") },
+                }
+            }
+
+            if 0 == win_count {
+                0
+            } else {
+                2u64.pow(win_count as u32 - 1)
+            }
         })
         .sum();
+
+    let pt2: u64 = card_count.iter().map(|(_, v)| v).sum();
 
     Ok(Answer {
         pt1: pt1,
