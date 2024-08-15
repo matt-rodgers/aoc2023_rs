9b22e40e635fc60b0db45761f3b6ceec795dd71c
Matt Rodgers <mattrodgers31@gmail.com>
2024-08-15 18:24:58 +0100

    day08 pt2

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -2,10 +2,9 @@ use crate::util::answer::*;
 use crate::util::input;
 use anyhow::Result;
 use std::collections::HashMap;
+use num::integer::lcm;
 
 pub fn run(input_path: &str) -> Result<Answer> {
-    let pt2 = 0;
-
     let lines = input::get_lines(input_path)?;
 
     let directions: Vec<usize> = lines[0]
@@ -34,21 +33,54 @@ pub fn run(input_path: &str) -> Result<Answer> {
     let mut i = 0;
     let mut current_node = "AAA";
 
-    while current_node != "ZZZ" {
-        let dir = directions[i % directions.len()];
-        i += 1;
+    if nodes.contains_key(current_node) {
+        /* Condition needed for testcase without AAA node for pt2 */
+        while current_node != "ZZZ" {
+            let dir = directions[i % directions.len()];
+            i += 1;
 
-        match nodes.get(current_node) {
-            Some(node) => current_node = node[dir],
-            None => {
-                panic!("Failed to get node {:?} from hashmap", current_node)
+            match nodes.get(current_node) {
+                Some(node) => current_node = node[dir],
+                None => {
+                    panic!("Failed to get node {:?} from hashmap", current_node)
+                }
             }
         }
     }
 
+    /* Part 2 */
+    let start_nodes: Vec<&str> = nodes
+        .keys()
+        .filter(|k| k.ends_with('A'))
+        .map(|k| *k)
+        .collect();
+
+    let loop_lengths: Vec<u64> = start_nodes.iter().map(|s| {
+            let mut j = 0;
+            let mut cur = *s;
+            
+            while !cur.ends_with('Z') {
+                let dir = directions[j % directions.len()];
+                
+                match nodes.get(cur) {
+                    Some(node) => { cur = node[dir] },
+                    None       => { panic!("Failed to get node {:?} from hashmap", cur) },
+                }
+                
+                j += 1;
+            }
+            
+            j as u64
+    }).collect();
+
+    let mut pt2 = loop_lengths[0];
+    for ll in loop_lengths.iter().skip(1) {
+        pt2 = lcm(pt2, *ll);
+    }
+
     Ok(Answer {
         pt1: i as u64,
-        pt2: pt2 as u64,
+        pt2: pt2,
     })
 }
 
@@ -60,13 +92,17 @@ mod tests {
     fn test_ex1() {
         let answer = run("inputs/08.ex1").unwrap();
         assert_eq!(answer.pt1, 2);
-        assert_eq!(answer.pt2, 0);
     }
 
     #[test]
     fn test_ex2() {
         let answer = run("inputs/08.ex2").unwrap();
         assert_eq!(answer.pt1, 6);
-        assert_eq!(answer.pt2, 0);
+    }
+
+    #[test]
+    fn test_ex3() {
+        let answer = run("inputs/08.ex3").unwrap();
+        assert_eq!(answer.pt2, 6);
     }
 }
